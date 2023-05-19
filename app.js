c8e73432211b766f5fb498d47252547beff4c368
Sarahina <maysarahina@gmail.com>
2023-05-19 13:50:37 -0400

    updated the codes

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 var g = G$('John', 'Doe');
-console.log(g);
+
+g.greet().setLang('es').greet(true).log();
+
+$('#login').click(function() {
+
+    var loginGrtr = G$('John', 'Doe');
+
+    $('#logindiv').hide();
+
+    loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+
+});
 
